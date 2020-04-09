7b6dd43d103e61be9da335332d55b7bf45bb62bb
Nhan Dang <nhantdang@gmail.com>
2020-04-09 12:31:06 +1000

    initial commit

---
+++ Conference_Track_Management_Tests/ProposalParserShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Conference_Track_Management_Tests
+{
+    public class ProposalParserShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
